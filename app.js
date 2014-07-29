30acb8d7cc589fe7bc33a4595384623f98a6e023
Michael Recachinas <mgr3yp@virginia.edu>
2014-07-28 21:14:25 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var request = require('request');
+var _		= require('rambda');
+var app 	= express();
+
+app.get('/', function(req, res) {
+	
+});
+
+app.listen('8081');
+console.log('Magic happens on port 8081');
+exports = module.exports = app;
