38d3a81b0e72251d46e8ce9f9048b30b8f11a5b1
Michael Recachinas <mgr3yp@virginia.edu>
2014-08-13 15:41:05 -0400

    remove dem tabs

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var request = require('request');
-var _		= require('lodash');
-var ramda	= require('ramda');
-var app 	= express();
+var _       = require('lodash');
+var ramda   = require('ramda');
+var app     = express();
 
 app.get('/', function(req, res) {
-	res.send('Hello World');
+    res.send('Hello World');
 });
 
 var server = app.listen(3000, function () {
-	console.log('Listening on port %d', server.address().port);
+    console.log('Listening on port %d', server.address().port);
 });
 exports = module.exports = app;
