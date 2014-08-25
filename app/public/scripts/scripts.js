00f6f03720d24e463c659a3c1b394660d77e1823
Michael Recachinas <mgr3yp@virginia.edu>
2014-08-25 09:14:20 -0400

    polishing pie chart

--- app/public/scripts/scripts.js
+++ app/public/scripts/scripts.js
@@ -51,16 +51,13 @@ Pie.prototype.plot = function (data) {
         var chart = nv.models.pieChart()
             .x(function(d) { return d.x })
             .y(function(d) { return d.y })
-            //.labelThreshold(.08)
-            //.showLabels(false)
+            .labelThreshold(.04)
+            // .showLabels(false)
             .showLegend(false)
             .color(d3.scale.category10().range())
             .width(width)
-            .height(height)
-            .donut(true);
-          // chart.pie.donutLabelsOutside(true).donut(true);
+            .height(height);
           d3.select("#pie")
-              //.datum(historicalBarChart)
               .datum(udata)
               .transition().duration(1200)
               .attr('width', width)
