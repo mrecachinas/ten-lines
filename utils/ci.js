52e44857d3d324a3cc2be01da28403f65bcd8c16
Kevin Welcher <kaw2k@virginia.edu>
2014-08-25 00:40:15 -0400

    Another

--- utils/ci.js
+++ utils/ci.js
@@ -6,9 +6,12 @@ var execFile = require('child_process').execFile;
 gith({
     repo: 'mrecachinas/ten-lines'
 }).on( 'all', function( payload ) {
+    console.log('hmm');
     if( payload.branch === 'master' ) {
+        console.log('cool');
         execFile('./build.sh', function(error, stdout, stderr) {
             console.log( 'exec complete' );
         });
     }
 });
+
