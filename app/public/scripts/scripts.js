981c89c318c3d787fc13b5bb7efd81d2c83c2a31
Michael Recachinas <mgr3yp@virginia.edu>
2014-08-26 00:42:03 -0400

    Bar is working...

--- app/public/scripts/scripts.js
+++ app/public/scripts/scripts.js
@@ -1,4 +1,11 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+var lodash = require('lodash');
+
+var match = {};
+var x_fmt = function (num) {
+    return match[num];
+}
+
 var Bar = function(data) {
     this.plot(data);
 };
@@ -6,23 +13,73 @@ var Bar = function(data) {
 Bar.prototype.crunch = function (data) {
     var dates = [];
     var count = 0;
-    for (d in data.byDate) {
-        var obj = {};
-        obj.x = k;
-        obj.y = 0;
+    for (u in data.byUser) {
+        var outer = {};
+        outer.key = u;
+        var values = [];
+        for (d in data.byUser[u]) {
+            var obj = {};
+            var intd = parseInt(d.split('-').join('')) || '';
+            obj.x = intd;
+            match[intd] = d;
+            obj.y = data.byUser[u][d].length;
+            values.push(obj);
+        }
+        outer.values = values;
+        dates.push(outer);
     }
+    for (var i = 0; i < dates.length; i++) {
+        dates[i].values = lodash.sortBy(dates[i].values, 'x');
+    }
+    window.dates = dates;
+    return dates;
 };
 
+/*
+[ 
+  {
+    key: USERNAME,
+    values: {
+      DATE: {
+        x: DATE,
+        y: LOC
+      }
+    } 
+  }
+]
+*/
+
 Bar.prototype.plot = function (data) {
-    var dates = this.crunch(data);
+    dates = this.crunch(data);
+    nv.addGraph(function() {
+        var chart = nv.models.multiBarChart()
+          .transitionDuration(350)
+          .reduceXTicks(true)   //If 'false', every single x-axis tick label will be rendered.
+          .rotateLabels(90)      //Angle to rotate x-axis labels.
+          .showControls(true)   //Allow user to switch between 'Grouped' and 'Stacked' mode.
+          .groupSpacing(0.1)    //Distance between each group of bars.
+          .showLegend(false);
+        ;
 
+        chart.xAxis
+            .tickFormat(x_fmt);
 
+        chart.yAxis
+            .tickFormat(d3.format(',r'));
 
+        d3.select('#bar')
+            .datum(dates)
+            .call(chart);
+
+        nv.utils.windowResize(chart.update);
+
+        return chart;
+    });
 
 };
 
 module.exports = Bar;
-},{}],2:[function(require,module,exports){
+},{"lodash":15}],2:[function(require,module,exports){
 var Pie = function(data) {
     this.plot(data);
 };
--- app/src/scripts/d3/bar.js
+++ app/src/scripts/d3/bar.js
@@ -1,3 +1,10 @@
+var lodash = require('lodash');
+
+var match = {};
+var x_fmt = function (num) {
+    return match[num];
+}
+
 var Bar = function(data) {
     this.plot(data);
 };
@@ -5,18 +12,68 @@ var Bar = function(data) {
 Bar.prototype.crunch = function (data) {
     var dates = [];
     var count = 0;
-    for (d in data.byDate) {
-        var obj = {};
-        obj.x = k;
-        obj.y = 0;
+    for (u in data.byUser) {
+        var outer = {};
+        outer.key = u;
+        var values = [];
+        for (d in data.byUser[u]) {
+            var obj = {};
+            var intd = parseInt(d.split('-').join('')) || '';
+            obj.x = intd;
+            match[intd] = d;
+            obj.y = data.byUser[u][d].length;
+            values.push(obj);
+        }
+        outer.values = values;
+        dates.push(outer);
     }
+    for (var i = 0; i < dates.length; i++) {
+        dates[i].values = lodash.sortBy(dates[i].values, 'x');
+    }
+    window.dates = dates;
+    return dates;
 };
 
+/*
+[ 
+  {
+    key: USERNAME,
+    values: {
+      DATE: {
+        x: DATE,
+        y: LOC
+      }
+    } 
+  }
+]
+*/
+
 Bar.prototype.plot = function (data) {
-    var dates = this.crunch(data);
+    dates = this.crunch(data);
+    nv.addGraph(function() {
+        var chart = nv.models.multiBarChart()
+          .transitionDuration(350)
+          .reduceXTicks(true)   //If 'false', every single x-axis tick label will be rendered.
+          .rotateLabels(90)      //Angle to rotate x-axis labels.
+          .showControls(true)   //Allow user to switch between 'Grouped' and 'Stacked' mode.
+          .groupSpacing(0.1)    //Distance between each group of bars.
+          .showLegend(false);
+        ;
 
+        chart.xAxis
+            .tickFormat(x_fmt);
 
+        chart.yAxis
+            .tickFormat(d3.format(',r'));
 
+        d3.select('#bar')
+            .datum(dates)
+            .call(chart);
+
+        nv.utils.windowResize(chart.update);
+
+        return chart;
+    });
 
 };
 
